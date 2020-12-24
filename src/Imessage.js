d53518da5b88cbd42ba73ef05ee4335989c3f128
vistritpandey <vistritpandey@gmail.com>
2020-12-24 14:50:57 +0530

    k

--- src/Imessage.js
+++ src/Imessage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Imessage.css";
 
 function Imessage() {
-  return <div></div>;
+  return (
+    <div className="imessage">
+      <h1>Hey guys</h1>
+    </div>
+  );
 }
 
 export default Imessage;
